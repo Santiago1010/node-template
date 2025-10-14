02fd4c93c7304f1dd40e0e13f4caf51d4b4b26ab
Santiago Correa Aguirre <santiago.c.a_10@hotmail.es>
2025-10-14 15:57:42 -0500

    feat(security): implement field-level authorization and audit tracking
    - Add field validation in pageUseEndpoint middleware to check authorized fields per endpoint
    - Create 'unauthorizedField' error message in both English and Spanish locales
    - Track field usage in configPagesEndpointsHasSchemas for audit purposes
    - Return 401 error for unauthorized field usage with detailed logging
    - Update existing field relationships when locations change and restore soft-deleted records
    - Include transaction safety across all database operations

--- middlewares/audit/pageEndpointLogger.middleware.js
+++ middlewares/audit/pageEndpointLogger.middleware.js
@@ -1,5 +1,10 @@
+const moment = require('moment');
+
 const ContextHelper = require('../../helpers/context.helper');
+const i18n = require('../../config/i18n');
 const { initializeConnection } = require('../../config/database/connection');
+const { perror } = require('../../helpers/debug.helper');
+const { error } = require('../../helpers/response.helper');
 
 const locations = ['body', 'params', 'query', 'header'];
 
@@ -9,12 +14,13 @@ const pageUseEndpoint = async (req, _, next) => {
 
   try {
     const sequelize = await initializeConnection();
-    const { configPagesHasEndpoints, configPagesEndpointsHasSchemas } = sequelize.models;
+    const { configPagesHasEndpoints, configPagesEndpointsHasSchemas, configEndpointsRequestSchema } = sequelize.models;
 
     await sequelize.transaction(async (transaction) => {
       let pageHasEndpoint = await configPagesHasEndpoints.findOne({
         where: { pageId: page.id, endpointId: endpoint.id },
         paranoid: false,
+        transaction,
       });
 
       if (!pageHasEndpoint) {
@@ -30,7 +36,41 @@ const pageUseEndpoint = async (req, _, next) => {
 
       for (const location of locations) {
         for (const field of Object.keys(req[location])) {
-          console.log(field);
+          const endpointField = await configEndpointsRequestSchema.findOne({
+            where: { endpointId: endpoint.id, name: field, location },
+            transaction,
+          });
+
+          if (!endpointField) {
+            perror('An attempt was made to make a request to an unknown endpoint field', {
+              endpoint: endpoint.path,
+              field,
+              location,
+              ip: req.ip,
+              timestamp: moment().format(
+                'dddd, DD [' + i18n.__('common.of') + '] MMMM [' + i18n.__('common.of') + '] YYYY, HH:mm:ss.SSS Z'
+              ),
+            });
+
+            throw error({ httpCode: 401, messagePath: 'errors.unauthorizedField', messageData: { field } });
+          }
+
+          const pageEndpointRequestSchema = await configPagesEndpointsHasSchemas.findOne({
+            where: { pageEndpointId: pageHasEndpoint.id, endpointFieldId: endpointField.id },
+            paranoid: false,
+            transaction,
+          });
+
+          if (!pageEndpointRequestSchema) {
+            await configPagesEndpointsHasSchemas.create(
+              { pageEndpointId: pageHasEndpoint.id, endpointFieldId: endpointField.id, location },
+              { transaction }
+            );
+          } else {
+            await pageEndpointRequestSchema.update({ location }, { transaction });
+
+            await pageEndpointRequestSchema.restore({ transaction });
+          }
         }
       }
     });
