e0fad0e8edbea529be1add151e0881a7a0b3fac9
Santiago Correa Aguirre <santiago.c.a_10@hotmail.es>
2025-10-14 13:13:25 -0500

    feat(security): enhance host validation with detailed error logging
    - Add comprehensive logging for unauthorized host access attempts
    - Include IP address and formatted timestamp in error reports
    - Integrate i18n support for localized timestamp formatting
    - Add 'unauthorized' error message key to both English and Spanish locales
    - Include 'of' translation for common date formatting usage

--- middlewares/context/contextBuilder.middleware.js
+++ middlewares/context/contextBuilder.middleware.js
@@ -1,6 +1,10 @@
+const moment = require('moment');
+
+const i18n = require('../../config/i18n');
 const ContextHelper = require('../../helpers/context.helper');
 const { initializeConnection } = require('../../config/database/connection');
 const { error } = require('../../helpers/response.helper');
+const { perror } = require('../../helpers/debug.helper');
 
 const setHost = async (req, _, next) => {
   const fullHost = req.protocol + '://' + req.get('host');
@@ -11,7 +15,17 @@ const setHost = async (req, _, next) => {
 
     let host = await configHosts.findOne({ attributes: ['id', 'url'], where: { url: fullHost }, raw: true });
 
-    if (!host) throw error({ httpCode: 401, messagePath: 'errors.unauthorized' });
+    if (!host) {
+      perror('An attempt was made to make a request from an unknown host', {
+        host: fullHost,
+        ip: req.ip,
+        timestamp: moment().format(
+          'dddd, DD [' + i18n.__('common.of') + '] MMMM [' + i18n.__('common.of') + '] YYYY, HH:mm:ss.SSS Z'
+        ),
+      });
+
+      throw error({ httpCode: 401, messagePath: 'errors.unauthorized' });
+    }
 
     return ContextHelper.run({ host }, () => next());
   } catch (error) {
