777baeecf3481eeca63d49e302d2d6c3c8ac55ab
Santiago Correa Aguirre <santiago.c.a_10@hotmail.es>
2025-10-25 11:29:29 -0500

    feat(i18n): add email template internationalization
    - Add hours translation keys for English and Spanish
    - Implement i18n in welcome email service with dynamic variables
    - Update email template to use localized content
    - Include support for pluralization in time displays
    - Add structured email content variables for better maintainability

--- services/emails/auth/session.email.js
+++ services/emails/auth/session.email.js
@@ -1,3 +1,4 @@
+const i18n = require('../../../config/i18n');
 const ContextHelper = require('../../../helpers/context.helper');
 const MailerHelper = require('../../../helpers/mailer.helper');
 
@@ -11,13 +12,40 @@ class SessionMailer extends MailerHelper {
   async sendWelcomeEmail(email, firstName, token) {
     const host = ContextHelper.get('host');
 
-    const url = `https://${host.url}/auth/verify-email?token=${token}`;
+    const url = `https://${host.url}/verify-email/${token}`;
 
     return await this.send({
       to: email,
-      subject: 'Bienvenido a nuestra aplicación',
+      subject: i18n.__('email.sendWelcomeEmail.subject'),
       templateName: '/auth/welcome',
-      variables: { firstName, token, url },
+      variables: {
+        welcomeGreeting: i18n.__mf('email.sendWelcomeEmail.welcomeGreeting', { firstName }),
+        paragraph1: i18n.__('email.sendWelcomeEmail.paragraph1'),
+        importantInformation: i18n.__('email.sendWelcomeEmail.importantInformation'),
+        paragraph2: i18n.__('email.sendWelcomeEmail.paragraph2'),
+        verifyMyEmail: i18n.__('email.sendWelcomeEmail.verifyMyEmail'),
+        whatYouDo: i18n.__('email.sendWelcomeEmail.whatYouDo'),
+        explorePanel: {
+          title: i18n.__('email.sendWelcomeEmail.explorePanel.title'),
+          paragraph: i18n.__('email.sendWelcomeEmail.explorePanel.paragraph'),
+        },
+        configProfile: {
+          title: i18n.__('email.sendWelcomeEmail.configProfile.title'),
+          paragraph: i18n.__('email.sendWelcomeEmail.configProfile.paragraph'),
+        },
+        reminder: {
+          title: i18n.__('email.sendWelcomeEmail.reminder.title'),
+          paragraph: i18n.__mf('email.sendWelcomeEmail.reminder.paragraph', { hours: i18n.__n('common.hours', 1) }),
+        },
+        questions: i18n.__('email.sendWelcomeEmail.questions'),
+        contactSupport: i18n.__('email.sendWelcomeEmail.contactSupport'),
+        alternative: {
+          button: i18n.__('email.sendWelcomeEmail.alternative.button'),
+          copyPaste: i18n.__('email.sendWelcomeEmail.alternative.copyPaste'),
+        },
+        thanks: i18n.__('email.sendWelcomeEmail.thanks'),
+        url,
+      },
       locale: 'es',
     });
   }
