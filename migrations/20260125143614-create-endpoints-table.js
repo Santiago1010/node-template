f2292f60fb9848c9f3879eddb960cb4c7bf8ed01
Santiago Correa Aguirre <santiago.c.a_10@hotmail.es>
2026-01-25 09:54:51 -0500

    feat(db): add endpoint configuration schema tables
    - Create config_endpoints table with method, platform, version and path fields
    - Create config_endpoints_has_required_scopes junction table with foreign keys
    - Create config_endpoints_request_schema table for parameter definitions
    - Add unique constraints and indexes for data integrity
    - Update sequelizerc configuration to use new migrations directory

---
+++ migrations/20260125143614-create-endpoints-table.js
@@ -0,0 +1,98 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable(
+      'config_endpoints',
+      {
+        id: {
+          type: Sequelize.INTEGER,
+          primaryKey: true,
+          autoIncrement: true,
+          allowNull: false,
+          comment: 'Unique primary key to identify each endpoint.',
+        },
+        method: {
+          type: Sequelize.ENUM('post', 'get', 'put', 'patch', 'delete', 'options'),
+          allowNull: false,
+          comment: 'Method of the endpoint to which permission will be granted.',
+        },
+        platform: {
+          type: Sequelize.STRING(50),
+          allowNull: false,
+          comment: 'Target platform for the endpoint configuration',
+        },
+        version: {
+          type: Sequelize.STRING(10),
+          allowNull: false,
+          comment: 'Version identifier of the endpoint configuration',
+        },
+        endpoint_group: {
+          type: Sequelize.STRING(100),
+          allowNull: false,
+          comment: 'Grouping of different endpoints',
+        },
+        path: {
+          type: Sequelize.STRING(200),
+          allowNull: false,
+          comment: 'Path of the endpoint to which permission will be granted.',
+        },
+        description: {
+          type: Sequelize.TEXT,
+          allowNull: true,
+          comment: "Optional description of the endpoint's function.",
+        },
+        requires_authorization: {
+          type: Sequelize.BOOLEAN,
+          allowNull: false,
+          defaultValue: true,
+          comment: 'Indicates whether or not the endpoint requires authorization to be executed.',
+        },
+        has_sensitive_information: {
+          type: Sequelize.BOOLEAN,
+          allowNull: false,
+          defaultValue: false,
+          comment:
+            'Indicates whether the page contains sensitive information. Useful for defining what is and is not allowed in "safe mode."',
+        },
+        created_at: {
+          type: Sequelize.DATE,
+          allowNull: false,
+          defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+          comment: 'Date and time when the record was created in the table.',
+        },
+        updated_at: {
+          type: Sequelize.DATE,
+          allowNull: false,
+          defaultValue: Sequelize.literal('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'),
+          comment: 'Date and time when the record was last modified.',
+        },
+        deleted_at: {
+          type: Sequelize.DATE,
+          allowNull: true,
+          defaultValue: null,
+          comment:
+            'Date and time when the record was deactivated. If the value is null, it means the record is still active; otherwise, it indicates that the record has been deactivated (known as soft deletion), without removing the information from the table.',
+        },
+      },
+      {
+        engine: 'InnoDB',
+        charset: 'utf8mb4',
+        collate: 'utf8mb4_general_ci',
+        comment: 'Table with the permissions of a role with the endpoints.',
+      }
+    );
+
+    await queryInterface.addIndex('config_endpoints', ['method', 'platform', 'version', 'endpoint_group', 'path'], {
+      unique: true,
+      name: 'endpoint_UN',
+    });
+  },
+
+  async down(queryInterface) {
+    await queryInterface.removeIndex('config_endpoints', 'endpoint_UN');
+
+    await queryInterface.dropTable('config_endpoints');
+  },
+};
---
+++ migrations/20260125144915-create-endpoints-request-schema-table.js
@@ -0,0 +1,136 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable(
+      'config_endpoints_request_schema',
+      {
+        id: {
+          type: Sequelize.INTEGER,
+          primaryKey: true,
+          autoIncrement: true,
+          comment: 'Primary key. Unique auto-incrementing identifier for each request schema parameter record',
+        },
+        endpoint_id: {
+          type: Sequelize.INTEGER,
+          allowNull: false,
+          comment:
+            'Foreign key reference to the associated API endpoint. Identifies which endpoint this parameter belongs to',
+        },
+        security_level_id: {
+          type: Sequelize.INTEGER,
+          allowNull: true,
+          defaultValue: null,
+          comment: 'ID of the security level required to use this property on the endpoint.',
+        },
+        field_id: {
+          type: Sequelize.INTEGER,
+          allowNull: true,
+          defaultValue: null,
+          comment:
+            'ID of the field to which it belongs. This is used for cases where the field is an object or an array of objects.',
+        },
+        name: {
+          type: Sequelize.TINYTEXT,
+          allowNull: false,
+          comment:
+            'Canonical name of the request parameter as expected by the API (e.g. in URL, headers, or body). Case-sensitive',
+        },
+        location: {
+          type: Sequelize.ENUM('body', 'params', 'query', 'header', 'auth_token'),
+          allowNull: false,
+          comment: 'Field location in the request: body, parameters (path), query (URL), header, or auth_token',
+        },
+        data_type: {
+          type: Sequelize.ENUM('string', 'integer', 'boolean', 'array', 'object', 'file', 'float'),
+          allowNull: false,
+          comment: 'Expected data type for the parameter. Defines how the input should be parsed and validated',
+        },
+        is_required: {
+          type: Sequelize.BOOLEAN,
+          allowNull: false,
+          defaultValue: true,
+          comment: 'Indicates if the parameter is mandatory (TRUE) or optional (FALSE) for the request',
+        },
+        created_at: {
+          type: Sequelize.DATE,
+          allowNull: false,
+          defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+          comment: 'Date and time when the record was created in the table.',
+        },
+        updated_at: {
+          type: Sequelize.DATE,
+          allowNull: false,
+          defaultValue: Sequelize.literal('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'),
+          comment: 'Date and time when the record was last modified.',
+        },
+        deleted_at: {
+          type: Sequelize.DATE,
+          allowNull: true,
+          defaultValue: null,
+          comment:
+            'Date and time when the record was deactivated. If the value is null, it means the record is still active; otherwise, it indicates that the record has been deactivated (known as soft deletion), without removing the information from the table.',
+        },
+      },
+      {
+        engine: 'InnoDB',
+        charset: 'utf8mb4',
+        collate: 'utf8mb4_general_ci',
+        comment: 'Defines request parameters for API endpoints.',
+      }
+    );
+
+    await queryInterface.addIndex('config_endpoints_request_schema', ['endpoint_id'], {
+      name: 'endpoint',
+    });
+
+    await queryInterface.addIndex('config_endpoints_request_schema', ['field_id'], {
+      name: 'field',
+    });
+
+    await queryInterface.addIndex('config_endpoints_request_schema', ['security_level_id'], {
+      name: 'security_level',
+    });
+
+    await queryInterface.addConstraint('config_endpoints_request_schema', {
+      fields: ['endpoint_id'],
+      type: 'foreign key',
+      name: 'config_endpoints_request_schema_ibfk_1',
+      references: {
+        table: 'config_endpoints',
+        field: 'id',
+      },
+      onDelete: 'CASCADE',
+      onUpdate: 'CASCADE',
+    });
+
+    await queryInterface.addConstraint('config_endpoints_request_schema', {
+      fields: ['field_id'],
+      type: 'foreign key',
+      name: 'config_endpoints_request_schema_ibfk_2',
+      references: {
+        table: 'config_endpoints_request_schema',
+        field: 'id',
+      },
+      onDelete: 'CASCADE',
+      onUpdate: 'CASCADE',
+    });
+
+    await queryInterface.addConstraint('config_endpoints_request_schema', {
+      fields: ['security_level_id'],
+      type: 'foreign key',
+      name: 'config_endpoints_request_schema_ibfk_3',
+      references: {
+        table: 'config_security_levels',
+        field: 'id',
+      },
+      onDelete: 'RESTRICT',
+      onUpdate: 'RESTRICT',
+    });
+  },
+
+  async down(queryInterface) {
+    await queryInterface.dropTable('config_endpoints_request_schema');
+  },
+};
